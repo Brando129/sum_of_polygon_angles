9b4af314c85c1c919dec923af2e0c1a177e36e74
Brandon Colangelo <brandoncancode@gmail.com>
2023-05-07 10:25:01 -0400

    Initial Commit

---
+++ script.js
@@ -0,0 +1,8 @@
+// Given an n-sided regular polygon n, return the total sum of internal angles (in degrees).
+
+function internalAnglesSum(sides){
+var sum = (sides - 2) * 180
+    return sum
+}
+
+console.log(internalAnglesSum(6))
